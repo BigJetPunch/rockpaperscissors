7d27a3e2a540c82960e79df76133ff542642417b
BigJetPunch <801gale.zach@gmail.com>
2023-08-29 18:33:37 -0600

    Merged from Main
    Merged from main, edited README from online.
    Commiting notations to script.js
    creating player input and playing a round of Rock Paper Scissors

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # rockpaperscissors
 First JS project, creating console game for Rock paper scissors, creating GUI later
 
-Create a console-level game where you compete against a computer
+Create a console-level game where you compete against a computer.
 
 Setup game in console, return later for GUI creation
+
+Things learned
+Creating random selection
+Various ways to use git
--- script.js
+++ script.js
@@ -7,7 +7,17 @@ return choice;
 function playRound(playerSelection, computerSelection) {
 
 }
+/*Play a round
+Computer input > lowercase
+Player input > lowercase
 
+if same = tie
+Do I group them in lose and win? Hmm.
+*/
 const playerSelection = "rock";
+//Change later to accept prompt input
 const computerSelection = getComputerChoice();
+
 console.log(playRound(playerSelection,computerSelection));
+//use this to test, make sure you're getting player selection and
+//computer selection

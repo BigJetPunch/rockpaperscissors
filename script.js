db60b3ddc5066b882cdf03ea3a928c24d2041f11
BigJetPunch <801gale.zach@gmail.com>
2023-08-29 18:24:13 -0600

    Create playRound Fx
    Pseudo coding input for playing a round. Deciding how best to approach comparing player and computer inputs.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello!");
+function getComputerChoice() {
+    let options = ["Rock", "Paper", "Scissors"];
+let choice = options[Math.floor(Math.random()*options.length)];
+return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));

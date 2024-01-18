003425696b1ced4f22c408b0ab1afaacf46d6ba7
BigJetPunch <801gale.zach@gmail.com>
2024-01-17 17:30:59 -0700

    create player choice Function for player's choice of rock, paper, or scissors. made case-insensitive, only allows the three selections to go through. Removed old code for playRound(), mistakes were made and on review will need to start over.

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
 function getComputerChoice() {
-    let options = ["Rock", "Paper", "Scissors"];
+    let options = ["rock", "paper", "scissors"];
 let choice = options[Math.floor(Math.random()*options.length)];
 return choice;
 }
 
-function playRound(playerSelection, computerSelection) {
-
+function getPlayerSelection() {
+    let playerChoice = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
+    if (playerChoice == "rock"){
+return playerChoice;
+}   else if (playerChoice == "paper"){
+return playerChoice;
+}   else if (playerChoice == "scissors"){
+return playerChoice;
+}   else {
+return alert("Please refresh and select again.");
+}    
 }
-/*Play a round
-Computer input > lowercase
-Player input > lowercase
 
-if same = tie
-Do I group them in lose and win? Hmm.
-*/
-const playerSelection = "rock";
-//Change later to accept prompt input
+function playRound(playerSelection, computerSelection) {
+// Code for comparing computer and player selection goes here
+}
+
+let playerSelection = getPlayerSelection();
 const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection,computerSelection));
-//use this to test, make sure you're getting player selection and
-//computer selection
+
+
+
+
+
+
+//Current issue** 
+//error with .toLowerCase at the end of selection, figure out why later
+
+
+
+
+
+/*
+notes
+For playRound function 
+Get Computerselection - lowercase
+Get Playerselection - lowercase
+(if)
+Compare for all player loses, use %% and ||
+: ex computer rock && player scissors || computer... ect
+(else if)
+Compare for player wins
+(else)
+draw
+
+Return should have (winner var) beats (loser var), (you win or you lose)
+*/
